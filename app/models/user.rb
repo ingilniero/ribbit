5651cec79e738433f861808dacd2404123f5acd0
Gilberto Villa <ingilniero@gmail.com>
2013-09-28 23:14:29 -0500

    Add User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
   attr_accessible :avatar_url, :bio,
     :email, :name, :password, :password_confirmation, :username
   has_secure_password
+
+  validates :email, presence: true, uniqueness: true,
+    format: { with: /^[\w\.+-]+@([\w]+\.)+[a-zA-Z]+$/ }
+
+  validates :username, presence: true, uniqueness: true
+  validates :name, presence: true
 end
