011216341bf333fcf2be98f198f3b5b4809a0253
Gilberto Villa <ingilniero@gmail.com>
2013-09-28 23:11:52 -0500

    Add password and password_confirmation to the accesibles attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :avatar_url, :bio, :email, :name, :password_digest, :username
+  attr_accessible :avatar_url, :bio,
+    :email, :name, :password, :password_confirmation, :username
+  has_secure_password
 end
