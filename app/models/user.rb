99fc4adc9377f803207bc86734dc053493a6d57f
Gilberto Villa <ingilniero@gmail.com>
2013-09-28 23:30:47 -0500

    Create avatar url before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
+require 'digest/md5'
+
 class User < ActiveRecord::Base
   attr_accessible :avatar_url, :bio,
     :email, :name, :password, :password_confirmation, :username
   has_secure_password
 
   before_validation :prep_email
+  before_save :create_avatar_url
 
   validates :email, presence: true, uniqueness: true,
     format: { with: /^[\w\.+-]+@([\w]+\.)+[a-zA-Z]+$/ }
@@ -15,5 +18,10 @@ class User < ActiveRecord::Base
   private
   def prep_email
     self.email = self.email.strip.downcase if self.email
+  end
+
+  def create_avatar_url
+    self.avatar_url =
+      "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
   end
 end
