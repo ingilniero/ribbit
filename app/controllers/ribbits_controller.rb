e55f39f57a0184e8d6939022879842637285de68
Gilberto Villa <ingilniero@gmail.com>
2013-09-29 11:15:02 -0500

    Add public rabbits page

--- app/controllers/ribbits_controller.rb
+++ app/controllers/ribbits_controller.rb
@@ -1,4 +1,9 @@
 class RibbitsController < ApplicationController
+  def index
+    @ribbits = Ribbit.all
+    @ribbit = Ribbit.new
+  end
+
   def create
     ribbit = Ribbit.new(params[:ribbit])
     ribbit.user_id = current_user.id
