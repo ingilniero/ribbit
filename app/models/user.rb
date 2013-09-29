03686e47fb480062465204f7f61899c1454fd0c0
Gilberto Villa <ingilniero@gmail.com>
2013-09-28 23:25:53 -0500

    Sanitaze email before validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,17 @@ class User < ActiveRecord::Base
     :email, :name, :password, :password_confirmation, :username
   has_secure_password
 
+  before_validation :prep_email
+
   validates :email, presence: true, uniqueness: true,
     format: { with: /^[\w\.+-]+@([\w]+\.)+[a-zA-Z]+$/ }
 
   validates :username, presence: true, uniqueness: true
   validates :name, presence: true
+
+
+  private
+  def prep_email
+    self.email = self.email.strip.downcase if self.email
+  end
 end
