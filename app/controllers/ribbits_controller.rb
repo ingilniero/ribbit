f884f56de3858f4cc8096f8c626c76ad563ef52c
Gilberto Villa <ingilniero@gmail.com>
2013-09-29 10:29:37 -0500

    Add saving ribbits functionality

--- app/controllers/ribbits_controller.rb
+++ app/controllers/ribbits_controller.rb
@@ -1,2 +1,9 @@
 class RibbitsController < ApplicationController
+  def create
+    ribbit = Ribbit.new(params[:ribbit])
+    ribbit.user_id = current_user.id
+
+    flash[:error] = "Your Ribbit was over 140 characters"  unless ribbit.save
+    redirect_to root_url
+  end
 end
